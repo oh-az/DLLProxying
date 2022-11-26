b4861ec22250359af7686597ddd7a99dc0a4d3cc
Aziz <74332587+oh-az@users.noreply.github.com>
2022-11-26 12:19:54 -0500

    Update check.py

--- DLLProxying/Modules/check.py
+++ DLLProxying/Modules/check.py
@@ -4,11 +4,13 @@ from colorama import Fore
 
 def checkFile():
 	global fileName
-	if len(sys.argv) != 2:
-	    print(Fore.BLUE + "Usage: " + sys.argv[0] + " <DLL>\n\n")
+	global directory
+	if len(sys.argv) != 3:
+	    print(Fore.BLUE + "Usage: " + sys.argv[0] + " <DLL> <Original DLL Directory>\nExamples:\npython dllProxy.py dpapi.dll SysWOW64\npython dllProxy.py cscapi.dll System32\n\n")
 	    quit()
 
 	fileName = sys.argv[1]
+	directory = sys.argv[2]
 	if not os.path.exists(fileName):
 	    print(Fore.RED +"ERROR! " + fileName + " DOES NOT EXIST..\n\n")
 	    quit()
